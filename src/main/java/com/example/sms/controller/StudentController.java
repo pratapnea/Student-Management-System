74a7b60c6d7641f4d4f11cd5dba087c9a8497f1f
pratapnea <sereversion@gmail.com>
2021-07-06 13:29:39 +0545

    student controller class updated

--- src/main/java/com/example/sms/controller/StudentController.java
+++ src/main/java/com/example/sms/controller/StudentController.java
@@ -1,12 +1,28 @@
 package com.example.sms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.sms.service.StudentService;
+
 @Controller
-@RequestMapping(name = "/api/v1/students")
 public class StudentController {
 	
+	private StudentService studentService;
+
+	public StudentController(StudentService studentService) {
+		super();
+		this.studentService = studentService;
+	}
 	
+	
+//	List all students
+	@GetMapping(name = "/students")
+	public String listStudents(Model model) {
+		model.addAttribute("students", studentService.getAllStudents());
+		return "students";
+	}
 
 }
